c76c6288fdb20306b54b1b74b3b377f850e6bc7a
mahsamnfpr <121118846+mahsamnfpr@users.noreply.github.com>
2023-01-09 20:36:30 +0330

    correct api connection

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function displayForecast(response) {
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
 
-  let forecastHTML = ` <div class="row">`;
+  let forecastHTML = `<div class="row">`;
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
@@ -50,7 +50,9 @@ function displayForecast(response) {
 function getforecast(coordinates) {
   console.log(coordinates);
   let apiKey = "66db35c65e549357f5c2f7ce9299df12";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  // let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -113,7 +115,7 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 
-displayForecast();
+// displayForecast();
 
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
